fd330c1a7e8aa5b5d690f77385f17fcb219df803
Arden Sinclair <general@ardensinclair.com>
2022-01-26 03:23:51 -0800

    feat: Initial commit 🎉

---
+++ env.ts
@@ -0,0 +1,19 @@
+import { loadEnvironment, snowflakeToBigint } from "@deps";
+
+export interface Config {
+	port: number;
+	token: string;
+	api: string;
+	id: bigint;
+	channel: bigint;
+}
+
+const env = loadEnvironment();
+
+export const config = <Config> {
+	port: parseInt(env.PORT, 10),
+	token: env.TOKEN,
+	api: env.API,
+	id: snowflakeToBigint(env.ID),
+	channel: snowflakeToBigint(env.CHANNEL),
+};
---
+++ mod.ts
@@ -0,0 +1,49 @@
+import { Application, createBot, Router, sendMessage, startBot } from "@deps";
+import { config } from "/env.ts";
+
+const server = new Application();
+const router = new Router();
+
+const bot = createBot({
+	token: config.token,
+	botId: config.id,
+	events: { ready: () => console.log("ready") },
+	intents: [],
+});
+
+router.post("/notes", async ({ request, response }) => {
+	const auth = request.headers.get("Authorization");
+	if (!auth) {
+		response.status = 401;
+		return;
+	}
+
+	const token = auth.split(" ")[1];
+	if (token !== config.api) {
+		response.status = 401;
+		return;
+	}
+
+	const body = request.body();
+	if (body.type !== "json") {
+		response.status = 400;
+		return;
+	}
+
+	const json = await body.value;
+	await sendMessage(bot, config.channel, { content: json.message });
+	response.status = 200;
+});
+
+server.use(router.routes());
+server.use(router.allowedMethods());
+
+await Promise.allSettled([
+	startBot(bot),
+	server.listen({
+		port: config.port,
+		secure: true,
+		certFile: "./***REMOVED***.pem",
+		keyFile: "./***REMOVED***.key",
+	}),
+]);
