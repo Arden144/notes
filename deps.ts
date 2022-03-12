e6213633117f0a0a1e83d20ba60487eac175480b
Arden Sinclair <general@ardensinclair.com>
2022-03-12 04:19:57 -0800

    Remove legacy Deno backend

--- deps.ts
+++
@@ -1,8 +0,0 @@
-export {
-	createBot,
-	sendMessage,
-	snowflakeToBigint,
-	startBot,
-} from "https://deno.land/x/discordeno@13.0.0-rc18/mod.ts";
-export { config as loadEnvironment } from "https://deno.land/x/dotenv@v3.1.0/mod.ts";
-export { Application, Router } from "https://deno.land/x/oak@v10.1.0/mod.ts";
